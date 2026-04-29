f147c06f065e3ee38ed8e69573ec9980e4acb44c
Noah Boothroyd <nkboothroyd@gmail.com>
2026-04-28 23:26:47 -0500

    futzing with the base setting

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-    base: '/nkboothroyd.github.io/',
+    base: 'https://nkboothroyd.github.io/',
     build: {
         assetsDir: '',
     },
